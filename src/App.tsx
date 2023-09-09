b7c7ae5e00cac2f43e28cb0f03683326e9138244
devlev1980 <devlev1980@gmail.com>
2023-09-09 17:23:36 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import AllMeetupsPage from "./pages/AllMeetupsPage";
+import { Route, Routes } from "react-router-dom";
+import NewMeetupPage from "./pages/NewMeetupPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage />} />
+      <Route path="/new-meetup" element={<NewMeetupPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/NewMeetupPage.tsx
@@ -0,0 +1,10 @@
+import { Route } from "react-router-dom";
+
+const NewMeetupPage = () => {
+  return (
+    <>
+      <div>New Meetup Page</div>
+    </>
+  );
+};
+export default NewMeetupPage;
