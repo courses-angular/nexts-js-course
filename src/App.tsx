a40239146a3523e5e96e835c3ae52551cd9daa1c
devlev1980 <devlev1980@gmail.com>
2023-09-09 16:29:52 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/Overlay.tsx
+++
@@ -1,8 +0,0 @@
-interface BackdropProps {
-  onClick: () => void;
-}
-
-const Overlay = ({ onClick }: BackdropProps) => {
-  return <div className="backdrop" onClick={onClick} />;
-};
-export default Overlay;
